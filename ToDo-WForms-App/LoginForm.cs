5a6136243c2ff31ec201b0a8a1f8b1acb01c8b28
RadekHerman <107422011+RadekHerman@users.noreply.github.com>
2025-02-15 12:56:54 +0100

    feat: login in schema

--- ToDo-WForms-App/LoginForm.cs
+++ ToDo-WForms-App/LoginForm.cs
@@ -1,3 +1,5 @@
+using System.Drawing.Text;
+
 namespace ToDo_WForms_App
 {
     public partial class LoginForm : Form
@@ -27,14 +29,40 @@ namespace ToDo_WForms_App
             {
                 this.Show();
             }
-
         }
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            this.DialogResult = DialogResult.OK;
-            //this.DialogResult = DialogResult.Retry;
-            this.Close();
+            if ((!string.IsNullOrWhiteSpace(txtLogUsername.Text)) && (!string.IsNullOrWhiteSpace(txtLogPassword.Text)))
+            {
+                if (validateUser(txtLogUsername.Text, txtLogPassword.Text))
+                {
+                    this.DialogResult = DialogResult.OK;
+                    this.Close();
+                    MessageBox.Show("Login successful!");
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Username or Password!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid data! Username and Password are required!");
+            }
+        }
+
+        private bool validateUser(string username, string password)
+        {
+            using (var context = new ToDoDbContext())
+            {
+                bool userExists = context.Users.Any(u => u.Username == username);
+                if (!userExists) { return false; }
+                else
+                {
+                    return true;
+                }
+            }
         }
     }
 }

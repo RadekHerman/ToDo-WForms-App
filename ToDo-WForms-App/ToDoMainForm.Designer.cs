844de0b0e65f1e9b35cc53bd0577d0db5fe06235
107422011+RadekHerman@users.noreply.github.com <107422011+RadekHerman@users.noreply.github.com>
2025-02-26 00:45:01 +0100

    feat: starting DataGridView

--- ToDo-WForms-App/ToDoMainForm.Designer.cs
+++ ToDo-WForms-App/ToDoMainForm.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ToDoMainForm";
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(42, 75);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(664, 298);
+            dataGridView1.TabIndex = 0;
+            // 
+            // ToDoMainForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Name = "ToDoMainForm";
+            Text = "ToDoMainForm";
+            Load += ToDoMainForm_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
     }
 }
--- ToDo-WForms-App/ToDoMainForm.cs
+++ ToDo-WForms-App/ToDoMainForm.cs
@@ -1,12 +1,62 @@
-﻿namespace ToDo_WForms_App
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ToDo_WForms_App
 {
     public partial class ToDoMainForm : Form
     {
+        private int loggedInUserId;
+        private string loggedInUsername;
+
         public ToDoMainForm()
         {
             InitializeComponent();
-            int loggedInUserId = UserSession.UserId;
-            string loggedInUsername = UserSession.Username;
+            dataGridView1.AutoGenerateColumns = true;
+            loggedInUserId = UserSession.UserId;
+            loggedInUsername = UserSession.Username;
+            MessageBox.Show($"user ID {loggedInUserId}, user name: {loggedInUsername}");
         }
+
+        private void ToDoMainForm_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            MessageBox.Show("LoadData method called", "Debug");
+
+            using (var context = new ToDoDbContext())
+            {
+                MessageBox.Show("Database context initialized", "Debug");
+                var posts = context.Posts
+                    .Where(p => p.UserId == loggedInUserId)
+                    .Select(p => new
+                    {
+                        p.DateCreated,
+                        p.Subject,
+                        p.Content,
+                        p.DateTodo,
+                        p.HourTodo,
+
+                    })
+                    .ToList();
+
+                if (posts.Count == 0)
+                {
+                    MessageBox.Show("No posts found for the current user.", "Debug");
+                }
+                else
+                {
+                    MessageBox.Show($"Retrieved {posts.Count} posts for UserId: {loggedInUserId}", "Debug");
+                }
+
+                dataGridView1.DataSource = posts;
+            }
+
+        }
+
+
     }
 }

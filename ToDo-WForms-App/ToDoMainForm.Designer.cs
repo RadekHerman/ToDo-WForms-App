57c1b26ab5937014706bddfaca4b593f1fcbebde
RadekHerman <107422011+RadekHerman@users.noreply.github.com>
2025-02-26 17:48:54 +0100

    feat: created new post first step

--- ToDo-WForms-App/ToDoMainForm.Designer.cs
+++ ToDo-WForms-App/ToDoMainForm.Designer.cs
@@ -31,6 +31,12 @@
             dataGridView1 = new DataGridView();
             lblWelcome = new Label();
             btnAddTask = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            sqliteCommand1 = new Microsoft.Data.Sqlite.SqliteCommand();
+            dateTimePicker2 = new DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -61,18 +67,71 @@
             // 
             // btnAddTask
             // 
-            btnAddTask.Location = new Point(48, 40);
+            btnAddTask.Location = new Point(48, 46);
             btnAddTask.Name = "btnAddTask";
             btnAddTask.Size = new Size(75, 57);
             btnAddTask.TabIndex = 2;
             btnAddTask.Text = "Add Task";
             btnAddTask.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            label1.Location = new Point(137, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(442, 19);
+            label1.TabIndex = 3;
+            label1.Text = "Fill out the boxes and press the \"Add Task\" button to create a new task.";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(137, 74);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(159, 29);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(302, 74);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(159, 29);
+            textBox2.TabIndex = 4;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Location = new Point(467, 74);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(127, 29);
+            dateTimePicker1.TabIndex = 5;
+            // 
+            // sqliteCommand1
+            // 
+            sqliteCommand1.CommandTimeout = 30;
+            sqliteCommand1.Connection = null;
+            sqliteCommand1.Transaction = null;
+            sqliteCommand1.UpdatedRowSource = System.Data.UpdateRowSource.None;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Format = DateTimePickerFormat.Time;
+            dateTimePicker2.Location = new Point(614, 74);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.ShowUpDown = true;
+            dateTimePicker2.Size = new Size(91, 29);
+            dateTimePicker2.TabIndex = 5;
+            // 
             // ToDoMainForm
             // 
             AutoScaleDimensions = new SizeF(10F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(959, 451);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
             Controls.Add(btnAddTask);
             Controls.Add(lblWelcome);
             Controls.Add(dataGridView1);
@@ -90,5 +149,11 @@
         private DataGridView dataGridView1;
         private Label lblWelcome;
         private Button btnAddTask;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private DateTimePicker dateTimePicker1;
+        private Microsoft.Data.Sqlite.SqliteCommand sqliteCommand1;
+        private DateTimePicker dateTimePicker2;
     }
 }

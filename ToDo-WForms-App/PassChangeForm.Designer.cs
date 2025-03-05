5f96edfeadc830f9762d7c08d76f285a29a4bde0
RadekHerman <107422011+RadekHerman@users.noreply.github.com>
2025-03-05 16:35:06 +0100

    feat: PassChangeForm structure

--- ToDo-WForms-App/PassChangeForm.Designer.cs
+++ ToDo-WForms-App/PassChangeForm.Designer.cs
@@ -29,13 +29,22 @@
         private void InitializeComponent()
         {
             btnPassChangeConfirm = new Button();
+            lblPCUsername = new Label();
+            lblPCNewPassword = new Label();
+            lblPCRepeatPassword = new Label();
+            lblPCPassHelper = new Label();
+            label5 = new Label();
+            txtCPUsername = new TextBox();
+            tztPCPassHelper = new TextBox();
+            txtCPNewPassword = new TextBox();
+            txtCPRepeatPassword = new TextBox();
             SuspendLayout();
             // 
             // btnPassChangeConfirm
             // 
             btnPassChangeConfirm.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            btnPassChangeConfirm.Location = new Point(196, 242);
-            btnPassChangeConfirm.Margin = new Padding(4, 4, 4, 4);
+            btnPassChangeConfirm.Location = new Point(196, 266);
+            btnPassChangeConfirm.Margin = new Padding(4);
             btnPassChangeConfirm.Name = "btnPassChangeConfirm";
             btnPassChangeConfirm.Size = new Size(107, 32);
             btnPassChangeConfirm.TabIndex = 0;
@@ -43,21 +52,116 @@
             btnPassChangeConfirm.UseVisualStyleBackColor = true;
             btnPassChangeConfirm.Click += btnPassChangeConfirm_Click;
             // 
+            // lblPCUsername
+            // 
+            lblPCUsername.AutoSize = true;
+            lblPCUsername.Location = new Point(30, 68);
+            lblPCUsername.Name = "lblPCUsername";
+            lblPCUsername.Size = new Size(88, 21);
+            lblPCUsername.TabIndex = 1;
+            lblPCUsername.Text = "Username:";
+            // 
+            // lblPCNewPassword
+            // 
+            lblPCNewPassword.AutoSize = true;
+            lblPCNewPassword.Location = new Point(30, 109);
+            lblPCNewPassword.Name = "lblPCNewPassword";
+            lblPCNewPassword.Size = new Size(127, 21);
+            lblPCNewPassword.TabIndex = 2;
+            lblPCNewPassword.Text = "New Password:";
+            // 
+            // lblPCRepeatPassword
+            // 
+            lblPCRepeatPassword.AutoSize = true;
+            lblPCRepeatPassword.Location = new Point(30, 153);
+            lblPCRepeatPassword.Name = "lblPCRepeatPassword";
+            lblPCRepeatPassword.Size = new Size(145, 21);
+            lblPCRepeatPassword.TabIndex = 3;
+            lblPCRepeatPassword.Text = "Repeat Password:";
+            // 
+            // lblPCPassHelper
+            // 
+            lblPCPassHelper.AutoSize = true;
+            lblPCPassHelper.Font = new Font("Times New Roman", 14.25F, FontStyle.Italic, GraphicsUnit.Point, 238);
+            lblPCPassHelper.Location = new Point(30, 194);
+            lblPCPassHelper.Name = "lblPCPassHelper";
+            lblPCPassHelper.Size = new Size(164, 21);
+            lblPCPassHelper.TabIndex = 4;
+            lblPCPassHelper.Text = "A place in the word:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(31, 25);
+            label5.Name = "label5";
+            label5.Size = new Size(385, 21);
+            label5.TabIndex = 5;
+            label5.Text = "Please fill out the boxes to change your password.";
+            // 
+            // txtCPUsername
+            // 
+            txtCPUsername.Location = new Point(196, 66);
+            txtCPUsername.Name = "txtCPUsername";
+            txtCPUsername.Size = new Size(213, 29);
+            txtCPUsername.TabIndex = 6;
+            // 
+            // tztPCPassHelper
+            // 
+            tztPCPassHelper.Location = new Point(196, 186);
+            tztPCPassHelper.Name = "tztPCPassHelper";
+            tztPCPassHelper.Size = new Size(213, 29);
+            tztPCPassHelper.TabIndex = 6;
+            // 
+            // txtCPNewPassword
+            // 
+            txtCPNewPassword.Location = new Point(196, 101);
+            txtCPNewPassword.Name = "txtCPNewPassword";
+            txtCPNewPassword.PasswordChar = '*';
+            txtCPNewPassword.Size = new Size(213, 29);
+            txtCPNewPassword.TabIndex = 6;
+            // 
+            // txtCPRepeatPassword
+            // 
+            txtCPRepeatPassword.Location = new Point(196, 145);
+            txtCPRepeatPassword.Name = "txtCPRepeatPassword";
+            txtCPRepeatPassword.PasswordChar = '*';
+            txtCPRepeatPassword.Size = new Size(213, 29);
+            txtCPRepeatPassword.TabIndex = 6;
+            // 
             // PassChangeForm
             // 
             AutoScaleDimensions = new SizeF(10F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(520, 340);
+            Controls.Add(txtCPRepeatPassword);
+            Controls.Add(tztPCPassHelper);
+            Controls.Add(txtCPNewPassword);
+            Controls.Add(txtCPUsername);
+            Controls.Add(label5);
+            Controls.Add(lblPCPassHelper);
+            Controls.Add(lblPCRepeatPassword);
+            Controls.Add(lblPCNewPassword);
+            Controls.Add(lblPCUsername);
             Controls.Add(btnPassChangeConfirm);
             Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 238);
-            Margin = new Padding(4, 4, 4, 4);
+            Margin = new Padding(4);
             Name = "PassChangeForm";
             Text = "PassChangeForm";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button btnPassChangeConfirm;
+        private Label lblPCUsername;
+        private Label lblPCNewPassword;
+        private Label lblPCRepeatPassword;
+        private Label lblPCPassHelper;
+        private Label label5;
+        private TextBox txtCPUsername;
+        private TextBox tztPCPassHelper;
+        private TextBox txtCPNewPassword;
+        private TextBox txtCPRepeatPassword;
     }
 }

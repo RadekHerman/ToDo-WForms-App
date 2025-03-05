20b1672967207897746979c1988d02ed2471b179
RadekHerman <107422011+RadekHerman@users.noreply.github.com>
2025-03-05 13:30:31 +0100

    feat: PassChangeForm created

---
+++ ToDo-WForms-App/PassChangeForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDo_WForms_App
+{
+    public partial class PassChangeForm : Form
+    {
+        public PassChangeForm()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
